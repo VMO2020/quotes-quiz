b97732a1b003ae9f6752af94611eb93b31161002
VICTOR M OTTATI <victor.ottati@gmail.com>
2023-04-20 16:16:19 +0100

    ADD: dark mode

---
+++ js/dark.js
@@ -0,0 +1,31 @@
+'use strict';
+console.log('Dark javascript is working!');
+
+let flag = true;
+
+// Selectors
+const darkButton = document.querySelector('#dark');
+const mainContainer = document.querySelector('.main-container');
+
+// Content
+darkButton.textContent = '🌒';
+
+// Function
+function darkMode() {
+	// console.log('Click');
+	if (flag === true) {
+		darkButton.textContent = '☀️';
+		mainContainer.style.backgroundColor = 'black';
+		// logo.style.backgroundColor = 'transparent';
+		mainContainer.style.color = 'white';
+	} else {
+		darkButton.textContent = '🌒';
+		mainContainer.style.backgroundColor = 'white';
+		// logo.style.backgroundColor = 'white';
+		mainContainer.style.color = 'black';
+	}
+	flag = !flag;
+}
+
+// eventListener
+darkButton.addEventListener('click', darkMode);
